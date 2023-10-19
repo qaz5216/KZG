3e9d950829f7a20bfef43439b1a93781a53d8b7e
Park-Kyoung-Min <70632234+qaz5216@users.noreply.github.com>
2023-10-19 10:16:32 +0900

    enemybasic

---
+++ Source/KZG/EnemyFsm.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EnemyFsm.generated.h"
+UENUM(BlueprintType)
+
+enum class EEnemyState : uint8
+{/*-수면 상태
+		-단순 이동 / 순찰
+		- 인지(사운드 / 시각)
+		- 추적
+	*/
+	Idle UMETA(DisplayName = "Idle State"),
+	Move,
+	Tracking,
+	Attack,
+	Damage,
+	Die,
+	Sleep,
+	Search,
+};
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class KZG_API UEnemyFsm : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UEnemyFsm();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void IdleState();
+	void TrackingState();
+	void MoveState();
+	void AttackState();
+	void DamageState();
+	void DieState();
+	void SleepState();
+	void SearchState();
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="FSM")
+	EEnemyState mState=EEnemyState::Idle;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "FSM")
+	class AEnemy *Me;
+};
