b1092eb1eef957c404cd391fcb6a2d5a3d8852cf
Park-Kyoung-Min <70632234+qaz5216@users.noreply.github.com>
2023-10-23 17:36:02 +0900

    BasicClassAdd

---
+++ Source/KZG/Public/NetGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "NetGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KZG_API UNetGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/KZG/Public/NetGameStateBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "NetGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KZG_API ANetGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
