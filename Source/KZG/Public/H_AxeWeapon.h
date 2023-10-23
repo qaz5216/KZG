c380eb4577240c355f2e4b7b5da1982e7dbc6299
githeed <tmdgmldhk11@gmail.com>
2023-10-23 09:30:33 +0900

    weepon

---
+++ Source/KZG/Public/H_AxeWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "H_AttackWeapons.h"
+#include "H_AxeWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KZG_API AH_AxeWeapon : public AH_AttackWeapons
+{
+	GENERATED_BODY()
+	
+};
