d287297e02cecfc9ee57ae19d29f861453ec8546
Park-Kyoung-Min <70632234+qaz5216@users.noreply.github.com>
2023-10-27 14:49:10 +0900

    gogo

--- Source/KZG/Private/NetGameStateBase.cpp
+++ Source/KZG/Private/NetGameStateBase.cpp
@@ -2,4 +2,26 @@
 
 
 #include "NetGameStateBase.h"
+#include "GameFramework/PlayerState.h"
+#include <Net/UnrealNetwork.h>
 
+
+ANetGameStateBase::ANetGameStateBase()
+{
+	bReplicates = true;
+}
+
+TArray<APlayerState*> ANetGameStateBase::GetPlayerArrayByScore()
+{
+	TArray<APlayerState*> sortedPlayers = PlayerArray;
+
+	sortedPlayers.Sort([](const APlayerState& p1, const APlayerState& p2) { return p1.GetScore() > p2.GetScore(); });
+	return sortedPlayers;
+}
+
+void ANetGameStateBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ANetGameStateBase, bGameStart);
+}
